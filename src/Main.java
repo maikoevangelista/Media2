5f786097e2b9ed296258bba9113326f1b112386a
Maiko Evangelista <84150949+maikoevangelista@users.noreply.github.com>
2022-04-27 17:52:38 -0300

    fim

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         double nota1 = input.nextDouble();
         double nota2 = input.nextDouble();
         double nota3 = input.nextDouble();
-//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
         double media = ((nota1 *  2  ) + (nota2 *  3  ) + (nota3 * 5   )) / (2+3+5);
         System.out.printf("MEDIA = %.1f", media);
     }
